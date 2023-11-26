9495af9a92576befb4afe0fea2a85c2e72dad704
Dyphnah <nyandukodyphnah4@gmail.com>
2023-11-26 17:57:06 +0300

    first commit

--- app/app.py
+++ app/app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, jsonify, make_response
 from flask_migrate import Migrate
 
-from models import db, Restaurant
+from models import db, Restaurant, Pizza, RestaurantPizza
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/app.db'
@@ -13,10 +13,106 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
-def home():
-    return ''
+# routes
 
+@app.route('/restaurants', methods=['GET'])
+def get_restaurants():
+    restaurants = Restaurant.query.all()
+    restaurant_data = [
+        {'id': restaurant.id, 'name': restaurant.name, 'address': restaurant.address}
+        for restaurant in restaurants
+    ]
+    return jsonify(restaurant_data)
+
+@app.route('/restaurants/<int:restaurant_id>', methods=['GET'])
+def get_restaurant(restaurant_id):
+    restaurant = Restaurant.query.get(restaurant_id)
+
+    if not restaurant:
+        return make_response(jsonify({'error': 'Restaurant not found'}), 404)
+
+    pizzas = [{'id': pizza.id, 'name': pizza.name, 'ingredients': pizza.ingredients} for pizza in restaurant.pizzas]
+
+    restaurant_data = {
+        'id': restaurant.id,
+        'name': restaurant.name,
+        'address': restaurant.address,
+        'pizzas': pizzas
+    }
+
+    return jsonify(restaurant_data)
+
+@app.route('/restaurants/<int:restaurant_id>', methods=['DELETE'])
+def delete_restaurant(restaurant_id):
+    restaurant = Restaurant.query.get(restaurant_id)
+
+    if not restaurant:
+        return make_response(jsonify({'error': 'Restaurant not found'}), 404)
+
+    # Delete associated RestaurantPizza entries
+    RestaurantPizza.query.filter_by(restaurant_id=restaurant.id).delete()
+
+    # Delete the restaurant
+    db.session.delete(restaurant)
+    db.session.commit()
+
+    return '', 204  # Empty response with 204 status code
+
+@app.route('/pizzas', methods=['GET'])
+def get_pizzas():
+    pizzas = Pizza.query.all()
+    pizza_data = [
+        {'id': pizza.id, 'name': pizza.name, 'ingredients': pizza.ingredients}
+        for pizza in pizzas
+    ]
+    return jsonify(pizza_data)
 
 if __name__ == '__main__':
     app.run(port=5555)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from flask import Flask, make_response
+# from flask_migrate import Migrate
+
+# from models import db, Restaurant
+
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/app.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# migrate = Migrate(app, db)
+
+# db.init_app(app)
+
+# @app.route('/')
+# def home():
+#     return ''
+
+
+# if __name__ == '__main__':
+#     app.run(port=5555)
--- app/models.py
+++ app/models.py
@@ -1,10 +1,47 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
 
 db = SQLAlchemy()
 
 class Restaurant(db.Model):
-    __tablename__ = 'Restaurant'
+    __tablename__ = 'restaurant'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    address = db.Column(db.String(255), nullable=False)
 
-# add any models you may need. 
+    # Relationship between Restaurant and RestaurantPizza
+    pizzas = db.relationship('Pizza', secondary='restaurant_pizza', back_populates='restaurants')
+
+class Pizza(db.Model):
+    __tablename__ = 'pizza'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    ingredients = db.Column(db.String(255), nullable=False)
+
+    # Relationship between Pizza and RestaurantPizza
+    restaurants = db.relationship('Restaurant', secondary='restaurant_pizza', back_populates='pizzas')
+
+class RestaurantPizza(db.Model):
+    __tablename__ = 'restaurant_pizza'
+
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Float, nullable=False)
+
+    # foreign keys
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizza.id'), nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
+
+    # relationships
+    pizza = db.relationship('Pizza', back_populates='restaurant_pizzas')
+    restaurant = db.relationship('Restaurant', back_populates='pizza_pizzas')
+
+    # Validations
+    @validates('price')
+    def validate_price(self, key, value):
+        if not (1 <= value <= 30):
+            raise ValueError("Price must be between 1 and 30.")
+        return value
+
+
